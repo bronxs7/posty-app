25fd4a68e17f42c036f021a7ee333fe63f449c84
doman <domanpro7ect@gmail.com>
2021-02-24 02:58:24 +0700

    logging out & middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <nav class="p-6 bg-white flex justify-between mb-4">
       <ul class="flex items-center">
         <li>
-          <a href="" class="p-3">Home</a>
+          <a href="/" class="p-3">Home</a>
         </li>
         <li>
           <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
@@ -24,10 +24,13 @@
       <ul class="flex items-center">
         @auth
           <li>
-            <a href="" class="p-3">Doddy Arisman</a>
+          <a href="" class="p-3">{{ auth()->user()->name }}</a>
           </li>
           <li>
-            <a href="" class="p-3">Logout</a>
+            <form action="{{ route('logout') }}" method="post" class="inline p-3">
+              @csrf
+              <button type="submit">Logout</button>
+            </form>
           </li>
         @endauth
           
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,23 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
 Route::get('/posts', function () {
     return view('posts.index');
